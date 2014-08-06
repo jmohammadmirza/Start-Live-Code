9a0118b1ce324351012dfd271fd3be6bd3f1d468
jmohammadmirza <mdaslameme@gmail.com>
2014-08-06 17:31:18 +0530

    Final live
    PHP code

---
+++ about/index.php
@@ -0,0 +1,42 @@
+<?php include('../includes/head.php'); ?>
+
+<div class="faq">
+	<h1>About Start Tank</h1>
+<p>
+PayPal India, in partnership with The Indus Entrepreneurs (TiE) Chennai Chapter, launched Start Tank, the startup incubator, at PayPal’s Chennai.Start Tank provides the early stage startup a professional co-working space that gives you easy access to key PayPal employees and the benefits of working side by side with many other startups in the same office.
+<br/><br />We'll get you the advice and expertise you need, access to soft mentorship in HR, technology trainings, talent acquisition best practices, team identification, leadership development, university relations Workplace logistics and lots more.
+</p>
+
+	
+<h2>Eligibility</h2>
+<ul>
+	<li>Formally incorporated pre-angel/pre-venture funded seed and early-stage technology startups</li>
+	<li>Applicants selected through a joint selection process between PayPal and TiE Chennai</li>
+	<li>Startups with a significant tech centric component that can benefit from PayPal’s technical mentors and TiE’s business mentorship</li>
+	<li>Companies whose founders are willing to commit full time to incubation</li>
+</ul>
+</p>
+
+<h2>Assistance to Startups</h2>
+<ul>
+<li>Technology mentorship – leveraging eBay Inc.’s global technology and product competency
+
+</li><li>Business mentorship – leveraging TiE and its seasoned entrepreneur pool
+
+</li><li>Office area of over 11,000 sq.ft., which includes a world-class boardroom, discussion room, and a state-of-the-art conference room to support multiple start-up companies
+
+</li><li>Access to soft mentorship in HR, technology trainings, talent acquisition best practices, team identification, leadership development, university relations
+
+</li><li>Workplace logistics such as cafeteria, transport, mail room, doctor’s room, mothers’ room, ATM, library, and fitness center</li>
+</ul>
+
+	
+	
+	
+</div><!-- /faq -->
+
+<div class="clr" style="margin:20px 0;"></div>
+
+
+<?php include('../includes/footer.php');  ?>
+
